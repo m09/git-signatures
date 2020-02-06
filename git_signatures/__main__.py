81c1bbdcc89cb7ba4a555c84dca1189e85714aeb
m09 <142691+m09@users.noreply.github.com>
2020-02-06 20:50:34 +0100

    Add signature resolving
    Signed-off-by: m09 <142691+m09@users.noreply.github.com>

--- git_signatures/__main__.py
+++ git_signatures/__main__.py
@@ -1,3 +1,29 @@
+from argparse import ArgumentParser
+from logging import _nameToLevel as logging_name_to_level
+from pathlib import Path
+
+from coloredlogs import install as coloredlogs_install
+
+from git_signatures.resolving import resolve
+
+
 def main() -> None:
     """Entry point of the git-signatures tool."""
-    pass
+    parser = ArgumentParser("git-signatures", description="Git Signatures Resolver.")
+    parser.add_argument("repositories", type=Path, help="Repositories to resolve.")
+    parser.add_argument(
+        "--log-level",
+        default="INFO",
+        choices=logging_name_to_level,
+        help="Logging verbosity.",
+    )
+    args = parser.parse_args()
+
+    coloredlogs_install(
+        level=args.log_level,
+        fmt="%(asctime)s %(name)10s %(message)s",
+        datefmt="%H:%M:%S",
+    )
+    delattr(args, "log_level")
+
+    resolve(**vars(args))
